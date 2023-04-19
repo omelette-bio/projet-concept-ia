052751cd2c5f678fc0005de1d670cd847cd659a8
omelette <omelettebio@proton.me>
2023-04-19 17:52:54 +0200

    modified:   xml2pdf_dot2xml.py modified:   xmls/Seaux.xml

--- xml2pdf_dot2xml.py
+++ xml2pdf_dot2xml.py
@@ -33,6 +33,8 @@ def xml_to_pdf(xml_file):
                   transitions[debut] = []
                transitions[debut].append(fin.strip())
    
+   print(transitions)
+   
    try :
       dot = os.open(dot_file, os.O_WRONLY | os.O_CREAT)
    except:
