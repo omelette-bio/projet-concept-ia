e2deb1ef35af8b2b47f5f69dcd016239779d4723
omelette <omelettebio@proton.me>
2023-05-22 17:49:53 +0200

    modified:   converter.py

--- converter.py
+++ converter.py
@@ -6,9 +6,9 @@ import argparse, os, sys
 def xml_to_dot(xml_file):
    tree = ET.parse(xml_file)
    root = tree.getroot()
-   transitions = {}
    
    file_name = xml_file[:-4]
+   
    if __name__ == "__main__":
       if args.save == "default":
          dot_file = file_name + ".dot"
@@ -18,7 +18,23 @@ def xml_to_dot(xml_file):
    
    else:
       dot_file = file_name + ".dot"
-      
+   
+   # ouverture du fichier dot, créé s'il n'existe pas, vidé s'il existe
+   try:
+      dot = os.open(dot_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+   except:
+      print("error while creating the dot file")
+      return
+   
+   # creation du style du graphe
+   try:
+      os.write(dot,bytes("digraph {\n", 'utf-8'))
+      os.write(dot, bytes("\tnode[shape=circle, style=filled, fillcolor=white, color=black, fontcolor=black, fontsize=12];\n", 'utf-8'))
+   except:
+      print("error while writing the dot file")
+      return
+   
+   # parcours de l'arbre xml pour récupérer les transitions
    for i in root:
       for j in i:
          if j.tag == "valmatrix":
@@ -26,30 +42,18 @@ def xml_to_dot(xml_file):
                l = len(k.text)
                debut = k.text[0:l//2].replace(" ", "")
                fin = k.text[l//2:l].replace(" ", "")
-               if debut not in transitions:
-                  transitions[debut] = []
-               transitions[debut].append(fin.strip())
-   
-   try :
-      dot = os.open(dot_file, os.O_WRONLY | os.O_CREAT)
+               try:
+                  os.write(dot, bytes("\t"+debut + " -> " + fin + "\n", 'utf-8'))
+               except:
+                  print("error while writing the dot file")
+                  return
+   try:            
+      os.write(dot,bytes("}", 'utf-8'))
    except:
-      print("error while creating the dot file")
+      print("error while writing the dot file")
+      return
    
-   #creating the style of the graph
-   os.write(dot,bytes("digraph {\n", 'utf-8'))
-   os.write(dot, bytes("\tnode[shape=circle, style=filled, fillcolor=white, color=black, fontcolor=black, fontsize=12];\n", 'utf-8'))
-
-   # crée le graphe à partir du dictionnaire de transitions
-   for i in transitions:
-      for j in transitions[i]:
-         os.write(dot, bytes("\t"+i + " -> " + j + "\n", 'utf-8'))
-         
-   os.write(dot,bytes("}", 'utf-8'))
-
    os.close(dot)
-
-
-
 
 
 
@@ -67,38 +71,62 @@ def dot_to_xml(dot_file):
    try:
       dot = os.open(dot_file, os.O_RDONLY)
    except:
-      print("The file does not exists")
+      print("Error while opening the dot file")
       return
 
-   if args.save == "default":
-      xml_file = dot_file[:-4] + ".xml"
+
+   file_name = dot_file[:-4]
+   
+   if __name__ == "__main__":
+      if args.save == "default":
+         xml_file = file_name + ".xml"
+      else:
+         xml_file = args.save + ".xml"
    else:
-      xml_file = args.save + ".xml"
+      xml_file = file_name + ".xml"
+
    
-   # reading the dot file to get the transitions
-   transitions = {}
    # splitting the file into a list of words with the help of the spaces and the new lines
-   dot_transitions = os.read(dot, 8192).decode('utf-8').split()
+   try:
+      dot_transitions = os.read(dot, 8192).decode('utf-8').split()
+   except:
+      print("Error while reading the dot file")
+      return
    
+   # ouvre un nouveau fichier xml, le crée s'il n'existe pas, le vide s'il existe
+   try:
+      xml = os.open(xml_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+   except:
+      print("Error while opening/creating the xml file")
+      return
+   
+   # écriture de l'en-tête du fichier xml
+   try:
+      os.write(xml, bytes("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", 'utf-8'))
+      os.write(xml, bytes("<instance format=\"Talos\">\n", 'utf-8'))
+      os.write(xml, bytes("\t<values>\n", 'utf-8'))
+      os.write(xml, bytes("\t\t<valmatrix>\n", 'utf-8'))
+   except:
+      print("Error while writing the xml file")
+      return
+   
+   # écriture des transitions dans le fichier xml
    for i in range(len(dot_transitions)):
       if dot_transitions[i] == "->":
-         if dot_transitions[i-1] not in transitions:
-            transitions[dot_transitions[i-1]] = []
-         transitions[dot_transitions[i-1]].append(dot_transitions[i+1])
-
-   # now we have the transitions in a dictionary, we can create the xml file
-   xml = os.open(xml_file, os.O_WRONLY | os.O_CREAT)
-   os.write(xml, bytes("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n", 'utf-8'))
-   os.write(xml, bytes("<instance format=\"Talos\">\n", 'utf-8'))
-   os.write(xml, bytes("\t<values>\n", 'utf-8'))
-   os.write(xml, bytes("\t\t<valmatrix>\n", 'utf-8'))
-   for i in transitions:
-      # seperating the number in i with a space
-      for j in transitions[i]:
-         os.write(xml, bytes("\t\t\t<data>" + seperate_number(i) + " " + seperate_number(j) + "</data>\n", 'utf-8'))
-   os.write(xml, bytes("\t\t</valmatrix>\n", 'utf-8'))
-   os.write(xml, bytes("\t</values>\n", 'utf-8'))
-   os.write(xml, bytes("</instance>\n", 'utf-8'))
+         try:
+            os.write(xml, bytes("\t\t\t<data>" + seperate_number(dot_transitions[i-1]) + " " + seperate_number(dot_transitions[i+1]) + "</data>\n", 'utf-8'))
+         except:
+            print("Error while writing the xml file")
+            return
+   
+   # écriture de la fin du fichier xml
+   try:
+      os.write(xml, bytes("\t\t</valmatrix>\n", 'utf-8'))
+      os.write(xml, bytes("\t</values>\n", 'utf-8'))
+      os.write(xml, bytes("</instance>\n", 'utf-8'))
+   except:
+      print("Error while writing the xml file")
+      return
    
    os.close(xml)
 
